58e103d9e0afef2d7feef20dbb138788ca7e7453
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-10-25 18:39:51 +0000

    Created Home, Home.ruleset_option, Home.saved_result, NewRuleset and 1 more; Edited settings and custom package dependencies; Deleted Form1

--- client_code/NewRuleset/__init__.py
+++ client_code/NewRuleset/__init__.py
@@ -1,8 +1,12 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import NewRulesetTemplate
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class NewRuleset(NewRulesetTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
