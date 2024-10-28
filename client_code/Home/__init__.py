8e443dfb51866997dd75c80412c23c2360c97f59
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-10-28 16:24:13 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -11,7 +11,13 @@ class Home(HomeTemplate):
     self.init_components(**properties)
       
     # Any code you write here will run before the form opens.
+    self.loadHistory()
 
+  def loadHistory(self):
+    
+
+  def addNewHistoryItem(self):
+  
   def new_ruleset_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
