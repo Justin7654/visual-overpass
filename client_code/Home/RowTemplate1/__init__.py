5d2e4ab60d858d5d86369894844d1356ea372e35
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-10-28 18:36:59 +0000

    Created Home.RowRulesetTemplate, Home.RowTemplate1 and Form1; Edited Home; Deleted Home.ruleset_option

--- client_code/Home/RowTemplate1/__init__.py
+++ client_code/Home/RowTemplate1/__init__.py
@@ -1,14 +1,18 @@
-from ._anvil_designer import ruleset_optionTemplate
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
 
-class ruleset_option(ruleset_optionTemplate):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
