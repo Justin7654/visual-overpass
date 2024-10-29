b6615be7114e817ac81f36b7f9e824cfca55ef46
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-10-29 14:34:05 +0000

    Created NewRuleset.rule_match_tag; Edited NewRuleset and Home

---
+++ client_code/NewRuleset/rule_match_tag/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import rule_match_tagTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class rule_match_tag(rule_match_tagTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
