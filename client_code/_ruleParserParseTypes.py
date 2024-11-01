7c4fc5ec66258103b6a2f7c7bb27f5ab2e8b8cbd
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-11-01 21:53:17 +0000

    Edited _ruleParserParseTypes

--- client_code/_ruleParserParseTypes.py
+++ client_code/_ruleParserParseTypes.py
@@ -1,11 +1,23 @@
-def match_tag():
+def match_tag(tags):
+    quary = ""
+    for tag in tags:
+        key = tag.get("key")
+        value = tag.get("value")
+        is_not = tag.get("not", False)
+        
+        if is_not:
+          quary += f'[!"{key}"="{value}"]'
+        else:
+          quary += f'["{key}"="{value}"]'
+    
+    # Join all parts with an empty string (no space) to form the Overpass API query
+    return quary
+
+def has_tag(list):
   pass
 
-def has_tag():
+def intersects(list):
   pass
 
-def intersects():
-  pass
-
-def OR():
+def OR(list):
   pass
