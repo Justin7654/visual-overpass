ac09279de20ca08aebb1ac383da842758ef506f7
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-11-15 19:01:27 +0000

    Created NewRuleset.rules.old.rule_or_group_old; Edited NewRuleset.rules.rule_or_group, NewRuleset.rules.rule_has_tag, NewRuleset.rules.rule_intersects and NewRuleset.rules.rule_match_tag; Deleted NewRuleset.rules.rule_or_group_old

--- client_code/NewRuleset/rules/rule_intersects/__init__.py
+++ client_code/NewRuleset/rules/rule_intersects/__init__.py
@@ -13,5 +13,15 @@ class rule_intersects(rule_intersectsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.tag = {
+      "type": "Intersects",
+      "not": False,
+      "group1": self.rule_group_1.rule_group,
+      "group2": self.rule_group_2.rule_group
+    }
 
     # Any code you write here will run before the form opens.
+
+  def notSwitch_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    self.tag["not"] = self.notSwitch.checked
--- client_code/NewRuleset/rules/rule_match_tag/__init__.py
+++ client_code/NewRuleset/rules/rule_match_tag/__init__.py
@@ -1,14 +1,8 @@
-from ._anvil_designer import rule_match_tag_oldTemplate
+from ._anvil_designer import rule_match_tagTemplate
 from anvil import *
-import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
-class rule_match_tag_old(rule_match_tag_oldTemplate):
+
+class rule_match_tag(rule_match_tagTemplate):
   def __init__(self, lastTag=False, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
