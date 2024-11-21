3cc58f76c12db153fe6c689173faf0d63776125b
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-11-21 20:09:07 +0000

    Edited NewRuleset.rules.rule_intersects

--- client_code/NewRuleset/rules/rule_intersects/__init__.py
+++ client_code/NewRuleset/rules/rule_intersects/__init__.py
@@ -17,7 +17,6 @@ class rule_intersects(rule_intersectsTemplate):
       "type": "Intersects",
       "not": False,
       "group1": self.rule_group_1.rule_group,
-      "group2": self.rule_group_2.rule_group
     }
 
     # Any code you write here will run before the form opens.
