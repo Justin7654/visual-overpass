594a93f3bd593a21402cac65f92713c59b5e7907
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-12-02 01:10:45 +0000

    Created RunRusult; Edited settings, custom package dependencies and Home; Deleted RunResult

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -27,6 +27,7 @@ class Home(HomeTemplate):
     open_form('NewRuleset', preset=False)
 
   def ruleset_datagrid_show(self, **event_args):
+    open_form("RunRusult")
     with anvil.server.loading_indicator(self.ruleset_repeating_panel, min_height=100):
       #self.loadRulesets()
       try:
--- client_code/RunRusult/__init__.py
+++ client_code/RunRusult/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RunResultTemplate
+from ._anvil_designer import RunRusultTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -7,11 +7,11 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import leaflet
 
-
-class RunResult(RunResultTemplate):
+class RunRusult(RunRusultTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
