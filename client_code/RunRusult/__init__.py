3f00926895ab5620b0045119c3a23fa1f92d83b9
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-12-02 15:15:57 +0000

    Edited RunRusult

--- client_code/RunRusult/__init__.py
+++ client_code/RunRusult/__init__.py
@@ -3,9 +3,9 @@ from anvil import *
 import anvil.server
 import anvil.users
 import anvil.tables as tables
+import anvil.js
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from anvil import js
 
 class RunRusult(RunRusultTemplate):
   def __init__(self, **properties):
@@ -13,3 +13,16 @@ class RunRusult(RunRusultTemplate):
     self.init_components(**properties)
    
     # Any code you write here will run before the form opens.
+
+  def form_show(self, **event_args):
+    """This method is called when the form is shown on the page"""
+    renderAt = anvil.js.get_dom_node(self.map_placeholder)
+    leaf = anvil.js.window.leaflet
+    map = leaf.map(renderAt).setView([51.505, -0.09], 13)
+    leaf.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      "maxZoom": 19,
+      "attribution": '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+    marker = leaf.marker([51.5, -0.09]).addTo(map)
+
+    
