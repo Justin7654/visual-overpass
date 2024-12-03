1906a86edd613560fb306044836dcb380538b245
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2024-12-03 16:56:42 +0000

    Edited RulesetResult

--- client_code/RulesetResult/__init__.py
+++ client_code/RulesetResult/__init__.py
@@ -17,13 +17,16 @@ class RulesetResult(RulesetResultTemplate):
     """This method is called when the form is shown on the page"""
     renderAt = anvil.js.get_dom_node(self.map_placeholder)
     leaf = anvil.js.window.leaflet
-    map = leaf.map(renderAt).setView([51.505, -0.09], 13)
+    map = leaf.map(renderAt).setView([0, 0], 1)
     leaf.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       "maxZoom": 19,
       "attribution": '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map)
     self.geoLayer = leaf.geoJSON(self.geojson)
     self.geoLayer.addTo(map)
+    #try:
+    map.fitBounds(self.geoLayer.getBounds())
+    #except:
     self.map = map
 
 
