ba4da5aaf013d56b9454681496d82a11bdc95b80
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2025-01-09 21:55:41 +0000

    Edited RunRuleset

--- client_code/RunRuleset/__init__.py
+++ client_code/RunRuleset/__init__.py
@@ -129,8 +129,10 @@ class RunRuleset(RunRulesetTemplate):
           print(err)
       elif state == "killed":
         Notification("Quary task was killed",title="error",style="warning",timeout=4).show()
+        self.errMessage = "Quary task was killed"
       elif state == "missing":
         Notification("Quary task went missing. This is most likely caused by an outage with our hosting provider",title="missing",timeout=4).show()
+        self.errMessage = "Quary task went missing"
       self.onTaskFail()
 
   def progressDots_tick(self, **event_args):
