c67866bfdbb7b60a6928b9414323ac30e4c39208
justinolsen2007@gmail.com <justinolsen2007@gmail.com>
2025-01-27 16:56:15 +0000

    Made getting the key list non-blocking

--- client_code/tagsManager.py
+++ client_code/tagsManager.py
@@ -1,14 +1,23 @@
 import anvil.server
+from anvil_extras.non_blocking import call_async
 
 keyList = {}
 valueLists = {}
 
-#Need to put inside a try statement or else designer wont work in forms where this is imported
-if len(keyList) == 0:
-  try:
-    keyList = anvil.server.call_s("getKeyList")
-  except anvil.server.SessionExpiredError:
-    pass
+def tryGetKeyList():
+  if len(keyList) > 0:
+    return print("Attempt to get key list when its already loaded!")
+  
+  #Handlers
+  def _ResultHandler(result):
+    global keyList
+    keyList = result
+    print("Got keylist")
+    
+  def _ErrorHandler():
+    print("Error getting key list")
+  #Call the server function with non blocking
+  call_async("getKeyList").on_result(_ResultHandler, _ErrorHandler)
 
 def getKeyList():
   return keyList
@@ -20,4 +29,11 @@ def getValueList(key):
   if valueLists.get(key) is None:
     valueLists[key] = anvil.server.call_s("getValueListForKey", key)
   return valueLists[key]
-  
+
+#Need to put inside a try statement or else designer wont work in forms where this is imported
+if len(keyList) == 0:
+  try:
+    tryGetKeyList()
+    #keyList = anvil.server.call_s("getKeyList")
+  except anvil.server.SessionExpiredError:
+    pass
